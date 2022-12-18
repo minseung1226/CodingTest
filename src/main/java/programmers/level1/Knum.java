85fc4c1337f3f988f5faa22f18aaf8ffeebcfd3b
minseung1226 <minseung1226@naver.com>
2022-12-18 18:08:01 +0900

    프로그래머스 level1 K번째 수

---
+++ src/main/java/programmers/level1/Knum.java
@@ -0,0 +1,28 @@
+package programmers.level1;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Spliterator;
+
+public class Knum {
+    public static void main(String[] args) {
+        //프로그래머스 Level1 K번째 수
+
+        int[] solution = solution(new int[]{1, 5, 2, 6, 3, 7, 4}, new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 2}});
+        for (int i =0;i<solution.length;i++){
+            System.out.print(solution[i]+", ");
+        }
+    }
+    public static int[] solution(int[] array,int[][] commands ){
+        int[] answer=new int[commands.length];
+
+        for(int i =0 ; i<commands.length;i++){
+            int[] test = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(test);
+            answer[i]=test[commands[i][2]-1];
+        }
+
+
+        return answer;
+    }
+}
