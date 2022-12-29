5bdf82dafbddd72af20b26b546755efa9e18b624
minseung1226 <minseung1226@naver.com>
2022-12-29 17:14:58 +0900

    프로그래머스 level1 x만큼 간격이 있는 n개의 숫자

---
+++ src/main/java/programmers/level1/Nnum.java
@@ -0,0 +1,16 @@
+package programmers.level1;
+
+public class Nnum {
+    public static void main(String[] args) {
+        //프로그래머스 level1 x만큼 간격이 있는 n개의 숫자
+    }
+    public static long[] solution(int x,int n){
+        long[] answer= new long[n];
+        int num=0;
+        for(int i=0;i<n;i++){
+            num+=x;
+            answer[i]=num;
+        }
+        return answer;
+    }
+}
