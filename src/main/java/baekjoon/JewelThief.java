ad8adba88e22f32630e23b44f709cbba6c896015
minseung1226 <minseung1226@naver.com>
2023-01-20 22:04:57 +0900

    백준 1202번 보석 도둑 시간초과..ㅜ

--- src/main/java/baekjoon/JewelThief.java
+++ src/main/java/baekjoon/JewelThief.java
@@ -1,45 +1,56 @@
 package baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.util.*;
 
 public class JewelThief {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //백준 1202번 보석 도둑
         Scanner scan = new Scanner(System.in);
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         int result=0;
-        int jewelCount=scan.nextInt();
-        int bagCount = scan.nextInt();
-        Jewel[] jewels=new Jewel[jewelCount];
-        Integer[] bags=new Integer[bagCount];
+        String[] jbCount = br.readLine().split(" ");
+        int jewelCount=Integer.parseInt(jbCount[0]);
+        int bagCount =Integer.parseInt(jbCount[1]);
 
-        for(int i = 0 ; i <jewels.length;i++){
+        PriorityQueue<Jewel> jewels = new PriorityQueue<>(Collections.reverseOrder());
+        List<Integer> bags = new ArrayList<>();
+
+        for(int i = 0 ; i <jewelCount;i++){
+            String[] jewelLine = br.readLine().split(" ");
             Jewel jewel = new Jewel();
-            jewel.weight=scan.nextInt();
-            jewel.price=scan.nextInt();
-            jewels[i]=jewel;
+            jewel.weight=Integer.parseInt(jewelLine[0]);
+            jewel.price=Integer.parseInt(jewelLine[1]);
+            jewels.add(jewel);
         }
-        for(int i = 0 ; i <bags.length;i++){
-            bags[i]=scan.nextInt() ;
+        for(int i = 0 ; i <bagCount;i++){
+            bags.add(Integer.parseInt(br.readLine()));
         }
+        Collections.sort(bags);
 
-        Arrays.sort(jewels, Collections.reverseOrder());
-        Arrays.sort(bags);
+        while (!jewels.isEmpty()) {
+            Jewel jewel = jewels.poll();
 
-            int i=0;
-            for( ; i <jewels.length;i++){
-                if(bags[bagCount-1]>=jewels[i].weight && jewels[i].weight>=0){
-                    result+=jewels[i].price;
-                    bagCount--;
-                    if(bagCount<=0) break;
+            int bagIndex = -1;
+            for (int i = 0; i < bags.size(); i++) {
+                if (bags.get(i) >= jewel.weight) {
+                    result += jewel.price;
+                    bags.remove(i);
+                    break;
                 }
+            }
 
+
+
+            if(bags.isEmpty()) break;
 
         }
+
 
         System.out.println(result);
-
-
     }
 
     static class Jewel implements Comparable<Jewel>{
