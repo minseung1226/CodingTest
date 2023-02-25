c800580c5490472add023a5689c42decf88afe1c
minseung1226 <minseung1226@naver.com>
2023-02-25 21:12:53 +0900

    프로그래머스 level2 호텔 대실

--- src/main/java/programmers/level2/HotelRoom.java
+++ src/main/java/programmers/level2/HotelRoom.java
@@ -9,51 +9,48 @@ import java.util.PriorityQueue;
 public class HotelRoom {
     // 프로그래머스 level2 호텔 대실
     public static void main(String[] args) {
+        String[][] strings = {{"02:00", "11:10"}, {"04:40", "05:20"}, {"06:20", "10:40"}, {"01:20", "13:20"}, {"08:20", "21:20"}};
+        System.out.println("solution1=" +solution(strings));
 
     }
 
-    public static int solution(String[][] book_time){
-        PriorityQueue<TimeClass> studyRoom=new PriorityQueue<>();
-        PriorityQueue<TimeClass> timeClasses=new PriorityQueue<>();
+    public static int solution(String[][] book_time){//(a,b)->a.start-b.start
+        PriorityQueue<TimeClass> studyRoom=new PriorityQueue<>((a,b)->a.end-b.end);
+        PriorityQueue<TimeClass> timeClasses=new PriorityQueue<>((a,b)->a.start-b.start);
 
         for(int i=0;i < book_time.length;i++){
             timeClasses.add(new TimeClass(book_time[i]));
         }
 
+
         while (!timeClasses.isEmpty()){
             TimeClass timeClass = timeClasses.poll();
-            if(studyRoom.isEmpty()){
-                studyRoom.add(timeClass);
-            }
-            else{
-                TimeClass peek = studyRoom.peek();
-                if(peek.end<=timeClass.start){
+            if(!studyRoom.isEmpty()&&studyRoom.peek().end<=timeClass.start){
                     studyRoom.poll();
-                }
-                studyRoom.add(timeClass);
-
             }
+                studyRoom.add(timeClass);
         }
         return studyRoom.size();
     }
-    static class TimeClass implements Comparable<TimeClass> {
+
+    static class TimeClass{
         int start;
         int end;
 
         public TimeClass(String[] book_time) {
             this.start = changeTime(book_time[0]);
+            System.out.println("start="+book_time[0]+" change="+start);
             this.end =changeTime(book_time[1])+10;
+            System.out.println("end="+book_time[1]+" change="+end);
+            System.out.println();
         }
 
         private int changeTime(String book_time) {
-            return Integer.parseInt(book_time.substring(0, 2)) * 60 + Integer.parseInt(book_time.substring(3));
+            String[] time = book_time.split(":");
+            return Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
         }
 
-
-
-        @Override
-        public int compareTo(TimeClass o) {
-            return this.end-o.end;
-        }
     }
+
+
 }
