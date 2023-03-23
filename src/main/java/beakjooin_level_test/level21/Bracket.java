3caf26b333ac48ed273261d92b3fa835c62c5582
minseung1226 <minseung1226@naver.com>
2023-03-23 23:50:37 +0900

    백준 9012번 괄호

--- src/main/java/beakjooin_level_test/level21/Bracket.java
+++ src/main/java/beakjooin_level_test/level21/Bracket.java
@@ -1,44 +1,48 @@
 package beakjooin_level_test.level21;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Bracket {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //9012번 괄호
 
         final String YES="YES";
         final String NO="NO";
 
-        Scanner scan=new Scanner(System.in);
-        Scanner scan2 = new Scanner(System.in);
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
 
-        int testCase = scan.nextInt();
+        int testCase = Integer.parseInt(br.readLine());
 
         String[] test=new String[testCase];
         for(int i=0;i<testCase;i++){
-            test[i]=scan2.nextLine();
+            test[i]=br.readLine();
         }
         String[] answer=new String[testCase];
 
-
-        System.out.println("test.length = " + test.length);
-        for (String s : test) {
-            System.out.println("s = " + s);
-        }
-
-        for(int i=0;i<test.length;i++){
-            String target = test[i];
+        A:for(int i=0;i<test.length;i++){
+            String word = test[i];
             Stack<Character> validStack = new Stack<>();
-            for(int j=0;j<target.length();j++){
-                if(validStack.isEmpty()){
-                    validStack.add(target.charAt(j));
-                }
-                else if(validStack.peek()==target.charAt(j)){
-                    validStack.add(target.charAt(i));
+            for(int j=0;j<word.length();j++){
+                char target = word.charAt(j);
+                if(target=='('){
+                    validStack.add(target);
                 }
                 else{
-                    validStack.pop();
+                    if(validStack.isEmpty()){
+                        answer[i]=NO;
+                        continue A;
+                    }
+                    else{
+                        if(validStack.peek()=='('){
+                            validStack.pop();
+                        }
+                        else{
+                            validStack.add(target);
+                        }
+                    }
                 }
             }
 
@@ -46,7 +50,7 @@ public class Bracket {
         }
 
         for (String ans : answer) {
-            System.out.println("ans = " + ans);
+            System.out.println(ans);
         }
 
     }
