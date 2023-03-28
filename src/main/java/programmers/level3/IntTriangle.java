7624c3a39de8cd976d9c47e2be322bdbda17df9e
minseung1226 <minseung1226@naver.com>
2023-03-28 16:57:16 +0900

    프로그래머스 level3 정수 삼각형 최적화

--- src/main/java/programmers/level3/IntTriangle.java
+++ src/main/java/programmers/level3/IntTriangle.java
@@ -1,11 +1,13 @@
 package programmers.level3;
 
+import java.util.Arrays;
+
 public class IntTriangle {
     public static void main(String[] args) {
 
 
         // level3 정수 삼각형
-        System.out.println(solution(new int[][]{{7},{3, 8},{8, 1, 0},{2, 7, 4, 4},{4, 5, 2, 6, 5}}));
+        System.out.println(solution2(new int[][]{{7},{3, 8},{8, 1, 0},{2, 7, 4, 4},{4, 5, 2, 6, 5}}));
 
 
     }
@@ -43,5 +45,18 @@ public class IntTriangle {
         }
 
         return answer;
+    }
+
+    public static int solution2(int[][] triangle) {
+        int answer = 0;
+
+        for (int i = 1; i < triangle.length; i++) {
+            triangle[i][0] += triangle[i - 1][0];
+            triangle[i][i] += triangle[i - 1][i - 1];
+            for (int j = 1; j < i; j++) {
+                triangle[i][j] += Math.max(triangle[i - 1][j], triangle[i - 1][j - 1]);
+            }
+        }
+        return Arrays.stream(triangle[triangle.length-1]).max().getAsInt();
     }
 }
