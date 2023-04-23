e5f6715dbac09c239f60df6c2cfbdef9dc0a3a74
minseung1226 <minseung1226@naver.com>
2023-04-23 19:24:25 +0900

    프로그래머스 level2 파일명 정렬 실패 ㅜ

---
+++ src/main/java/programmers/level2/FileNameSort.java
@@ -0,0 +1,46 @@
+package programmers.level2;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class FileNameSort {
+    public static void main(String[] args) {
+        //프로그래머스 level2 파일명 정렬
+        String[] solution = solution(new String[]{"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"});
+        for (String s : solution) {
+            System.out.println("s = " + s);
+        }
+
+    }
+
+    public static String[] solution(String[] files){
+        String[][] answer=new String[files.length][3];
+
+        for(int i=0;i<files.length;i++){
+            Matcher matcher=Pattern.compile("(\\D+)(\\d+)(.*)").matcher(files[i]);
+            if(matcher.find()){
+                answer[i][0]=matcher.group(1);
+                answer[i][1]=matcher.group(2);
+                answer[i][2]=matcher.group(3);
+            }
+        }
+
+        Arrays.sort(answer,(s1,s2)->{
+            if(s1[0].equals(s2[0])){
+                return Integer.parseInt(s1[1])-Integer.parseInt(s2[1]);
+            }
+            else{
+                return s1[0].toUpperCase().compareTo(s2[0].toUpperCase());
+            }
+        });
+
+        String[] strings = new String[answer.length];
+        for(int i=0;i<strings.length;i++){
+            strings[i]=answer[i][0]+answer[i][1]+answer[i][2];
+        }
+
+
+        return strings;
+    }
+}
